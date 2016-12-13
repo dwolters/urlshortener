f8bd6a5618969f7d2bcb81c3a3e7a119c4c9d263
Dennis Wolters <dbn@gmx.de>
2016-12-13 17:17:42 +0100

    Ensured idempotence of delete method

--- app.js
+++ app.js
@@ -91,14 +91,20 @@ app.delete('/:id', auth, (req, res) => {
         res.status(400).end('Invalid ID format');
         console.error('Request for invalid id:', id);
     } else {
-        fs.unlink(path.join(mappingPath, id))
+		var file = path.join(mappingPath, id);
+        fs.unlink(file)
             .then(() => {
                 delete mapping[id];
                 res.end('OK');
                 console.log('Delete short URL with id:', id);
             }, (err) => {
-                res.status(500).end('Internal Server Error');
-                console.error('Error during deletion:', err);
+				if(err.code === 'ENOENT') {
+                    res.end('OK');
+					console.log('Ignoring request: Short URL has already been deleted');
+				} else {
+                    res.status(500).end('Internal Server Error');
+				    console.error('Error during deletion:', err);
+				}
             });
     }
 });
